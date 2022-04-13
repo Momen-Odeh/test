02b94d087fbe66cb166ef1e3562bb60ea4b44d8a
Momen-Odeh <92532348+Momen-Odeh@users.noreply.github.com>
2022-04-13 15:37:58 +0300

    hi

--- src/test/Test.java
+++ src/test/Test.java
@@ -16,8 +16,8 @@ public class Test {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.err.println("dcbd");
-        System.out.println("");
+        System.err.println("dcbd"); 
+        System.out.println("dvrf");
     }
     
 }
